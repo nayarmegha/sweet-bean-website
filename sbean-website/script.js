e4afae9de4ae33ee2af82ec70a9d0269fbad5f5b
nayarmegha <me@meghanayar.com>
2023-04-23 16:15:55 -0500

    2nd check

--- sbean-website/script.js
+++ sbean-website/script.js
@@ -1,10 +1,15 @@
-var dayOfWeek = (new Date).getDay();
-var hours = ["SUN: 8 AM to 5 PM",    // Sunday
-             "MON: 8 AM to 5 PM",   // Monday
-             "TUE: 8 AM to 5 PM",   // Tuesday
-             "WED: 8 AM to 5 PM",   // Wednesday
-             "THUR: 8 AM to 5 PM",   // Thursday
-             "FRI: 8 AM to 5 PM",   // Friday
-             "SAT: 8 PM to 5 PM"];  // Saturday
-var todaysHours = hours[dayOfWeek];
-document.getElementById("hours").innerHTML = todaysHours;
+function checkIfOpen() {
+    var now = new Date();
+    var day = now.getDay();
+    var hour = now.getHours();
+  
+    var otime = 10;
+    var ctime = 24;
+    var msg = "we open at "+otime+"am";
+    var until = "midnight";
+  
+    if (day >= 1 && day <= 4) ctime = 22, until = "10pm";
+    if (hour >= otime && hour <= ctime) msg = "we are open until " + until;
+  
+    document.getElementById('areWeOpen').textContent = msg;
+};
